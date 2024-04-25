b15d0a5aa71c1f64ccf82614d15282427beab919
Michelle Duso <chelleduso@gmail.com>
2024-04-25 17:19:42 -0300

    adicionando minha url

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    API_BASE_URL: "https://api.typeform.com/",
+  },
+
+  experimentalSessionAndOrigin: true,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here

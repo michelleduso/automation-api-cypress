1cf91afb1a1233ae6b896409c845b854b53bf0da
Michelle Duso <chelleduso@gmail.com>
2024-04-26 11:08:54 -0300

    recuperando respostas do formulário

--- cypress/e2e/typeformAPI.cy.js
+++ cypress/e2e/typeformAPI.cy.js
@@ -9,11 +9,23 @@ describe('Typeform API Tests', () => {
             method: 'GET',
             url: `${API_URL}me`,
             headers: { authorization }
-        }).then(response => {
+        }).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.alias).to.eq(Cypress.env('userAlias'));
             expect(response.body.email).to.eq(Cypress.env('username'));
             expect(response.body.language).to.eq('en');
         });
+    });
+
+    it('retrieves form responses', () => {
+        cy.api({
+            method: 'GET',
+            url: `${API_URL}forms/${Cypress.env('formId')}/responses`,
+            headers: { authorization}
+        }).should(({ status, body }) => {
+            expect(status).to.eq(200);
+            expect(body.total_items).to.eq(body.items.length);           
+
+        })    
     });
 });
